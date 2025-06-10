d4c7c97ce1de8da33963273e015fc2b70bb0c2d3
Zain Shaikh <117446852+Zainfr@users.noreply.github.com>
2025-06-10 17:12:47 +0530

    Update page.tsx

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -24,7 +24,6 @@ const Explore = () => {
                     numSquares={30}
                     maxOpacity={0.1}
                     duration={3}
-                    repeatDelay={1}
                     className={cn(
                         "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
                         "skew-y-12"
